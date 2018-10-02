65cfcc50905791244fe577dfa19a626fa0711703
demego <gonvm1@gmail.com>
2018-10-02 16:44:31 +0100

    Added movement. Adjusted objects position.

--- Project1/js/chair.js
+++ Project1/js/chair.js
@@ -9,15 +9,16 @@ class Chair extends THREE.Object3D {
     this.addChairSit(0, 0, 0);
     this.addChairSupport(0, -1, 0);
 
-    this.addChairLeg1(-1, -16, 0);
-    this.addChairLeg1(9, -16, 0);
-    this.addChairLeg2(0, -16, -1);
-    this.addChairLeg2(0, -16, 9);
+    this.addChairLeg1(-1, -13, 0);
+    this.addChairLeg1(9, -13, 0);
+    this.addChairLeg2(0, -13, -1);
+    this.addChairLeg2(0, -13, 9);
 
-    this.addChairWheel(-9, -16, 1);
-    this.addChairWheel(7, -16, 1);
-    this.addChairWheel(-1, -16, 9);
-    this.addChairWheel(-1, -16, -7);
+    this.addChairWheel(-9, -13, 1);
+    this.addChairWheel(7, -13, 1);
+    this.addChairWheel(-1, -13, 9);
+    this.addChairWheel(-1, -13, -7);
+
 	}
 
   addChairSit(x, y, z) {
@@ -27,9 +28,9 @@ class Chair extends THREE.Object3D {
     this.add(mesh);
   }
   addChairSupport(x, y, z) {
-    var geometry = new THREE.CubeGeometry( 2, 15, 2);
+    var geometry = new THREE.CubeGeometry( 2, 12, 2);
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y-7.5, z);
+    mesh.position.set(x, y-6, z);
     this.add(mesh);
   }
   addChairLeg1(x, y, z) {
@@ -44,7 +45,7 @@ class Chair extends THREE.Object3D {
     mesh.position.set(x, y+1.5, z - 4);
     this.add(mesh);
   }
-  addChairWheel(obj, x, y, z) {
+  addChairWheel(x, y, z) {
     geometry = new THREE.TorusGeometry(1, 1, 16, 100);
     mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(x + 1, y-2, z-1);
@@ -57,5 +58,6 @@ class Chair extends THREE.Object3D {
     mesh.position.set(x, y+10, z+1);
     this.add(mesh);
   }
+
 
 }
--- Project1/js/lamp.js
+++ Project1/js/lamp.js
@@ -9,36 +9,37 @@ class Lamp extends THREE.Object3D {
      this.addLampAbajour(-30, 51, 0);
      this.addLampLight(-30, 51, 0);
      this.addLampSphere(-30, 53, 0);
+
   }
 
   addLampBase(x, y, z) {
     var geometry = new THREE.CylinderGeometry( 12, 12, 1, 25 );
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
+    mesh.position.set(x, y+0.5, z);
     this.add(mesh);
   }
   addLampSupport(x, y, z) {
     var geometry = new THREE.CylinderGeometry( 1, 1, 50, 25 );
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y + 25, z);
+    mesh.position.set(x, y + 25.5, z);
     this.add(mesh);
   }
   addLampAbajour(x, y, z) {
     var geometry = new THREE.CylinderGeometry( 5, 10, 12, 25 );
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y + 4, z);
+    mesh.position.set(x, y + 4.5, z);
     this.add(mesh);
   }
   addLampLight(x, y, z) {
     var geometry = new THREE.CylinderGeometry( 2, 0, 4, 25 );
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
+    mesh.position.set(x, y+0.5, z);
     this.add(mesh);
   }
   addLampSphere(x, y, z) {
     var geometry = new THREE.SphereGeometry(2, 26, 5, 0, 6, 0, 1.5);
     mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(x, y, z);
+    mesh.position.set(x, y+0.5, z);
     this.add(mesh);
   }
 
