0d5f2dc913b484b169384d9515e015dfae9dbda1
Mariana Saraiva <marianasaraiva@MacBook-Pro-de-Mariana.local>
2018-10-25 17:33:58 +0100

    aviao c cube p a meia esfera n ficar no ar; camara extra- p ver distancias

---
+++ Project3/js/plane.js
@@ -0,0 +1,46 @@
+'use strict'
+
+class Plane extends THREE.Object3D {
+
+	constructor(x,y,z) {
+		super();
+    this.mainPieceMaterial  = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+		this.wingMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
+		this.cockpitMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+
+		this.addMainPiece(0,0,0);
+		this.addWing(4, 0, 0);
+		this.addWing(-18, 0, 0);
+		this.addCockpit(0, 3, 7);
+	}
+
+	addMainPiece(x, y, z) {
+		var mainPiece = new THREE.Object3D();
+    var geometry = new THREE.CubeGeometry(6, 6, 30);
+    mesh = new THREE.Mesh(geometry, this.mainPieceMaterial);
+    mesh.position.set(x, y, z);
+		mesh.name = "mainPiece";
+    mainPiece.add(mesh);
+		this.add(mainPiece);
+	}
+
+	addWing(x, y, z){
+		var wing = new THREE.Object3D();
+		var geometry = new THREE.CubeGeometry(14, 1, 4);
+		mesh = new THREE.Mesh(geometry, this.wingMaterial);
+    mesh.position.set(x + 7, y, z);
+		mesh.name = "wing";
+    wing.add(mesh);
+		this.add(wing);
+}
+	addCockpit(x, y, z){
+		var cockpit = new THREE.Object3D();
+		var geometry = new THREE.SphereGeometry(3, 30, 15, 0, 6.3, 0, 1.6);
+    mesh = new THREE.Mesh(geometry, this.cockpitMaterial);
+    mesh.position.set(x, y, z);
+    mesh.name = "cockpit";
+    cockpit.add(mesh);
+		this.add(cockpit);
+	}
+
+}
---
+++ Project3/js/scene.js
@@ -0,0 +1,160 @@
+'use strict'
+
+var clock;
+var orthographicCamera, perspectiveCamera;
+var scene, renderer;
+var geometry, material, mesh;
+var deltaT;
+var sceneWidth = 3.8, sceneHeight = 3.8;
+var sceneRatio = sceneWidth / sceneHeight;
+var aspect;
+var rotateX = 0, rotateY = 0;
+var switchCamera = 0;
+var plane;
+
+var materials = [];
+
+
+function onResize() {
+
+  aspect = window.innerWidth / window.innerHeight;
+
+  perspectiveCamera.aspect = aspect;
+  perspectiveCamera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+
+function createCamera() {
+  aspect = window.innerWidth / window.innerHeight;
+  orthographicCamera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000);
+
+	orthographicCamera.position.set( 0, 6, 50 );
+	orthographicCamera.lookAt( scene.position );
+}
+
+function createPerspectiveCamera() {
+	perspectiveCamera = new THREE.PerspectiveCamera(
+                           70,
+                           window.innerWidth / window.innerHeight,
+                           1,
+                           1000);
+
+	perspectiveCamera.position.set( 30, 40, 30 );
+	perspectiveCamera.lookAt( scene.position );
+}
+
+
+function createScene() {
+
+  scene = new THREE.Scene();
+
+  scene.add( new THREE.AxesHelper(10) );
+  createPlane();
+}
+
+
+function createPlane( x,y,z ) {
+  plane = new Plane(0,0,0);
+  materials = [...materials, plane.mainPieceMaterial, plane.wingMaterial, plane.cockpitMaterial];
+  scene.add(plane);
+}
+
+
+
+function onKeyDown(e) {
+  switch ( e.keyCode ) {
+    case 65:  //A
+    case 97: //a
+      materials.forEach(function(material) {
+        material.wireframe = !material.wireframe;
+      });
+      break;
+    case 37:  // left
+      rotateY = 1;
+      console.log(`onKeyDown! Rotate left: ${switchCamera}`);
+      break;
+    case 38:  // up
+      rotateX = 1;
+      console.log(`onKeyDown! Rotate up: ${switchCamera}`);
+      break;
+    case 39:  // right
+      rotateY = 2;
+      console.log(`onKeyDown! Rotate right: ${switchCamera}`);
+      break;
+    case 40: // down
+      rotateX = 2;
+      console.log(`onKeyDown! Rotate down: ${switchCamera}`);
+      break;
+      case 49:  // 1
+        switchCamera = 1;
+        console.log(`onKeyDown! Switch to camera: ${switchCamera}`);
+        break;
+      case 50:  // 2
+        switchCamera = 2;
+        console.log(`onKeyDown! Switch to camera: ${switchCamera}`);
+        break;
+  }
+}
+
+
+function render() {
+  switch ( switchCamera ) {
+      case 1:
+
+        orthographicCamera.updateProjectionMatrix();
+        orthographicCamera.lookAt(scene.position);
+        renderer.render(scene, orthographicCamera);
+        break;
+
+      case 2:
+        perspectiveCamera.updateProjectionMatrix();
+        perspectiveCamera.lookAt(scene.position);
+        renderer.render(scene, perspectiveCamera);
+        break;
+
+      default:
+        renderer.render(scene, perspectiveCamera);
+    }
+  }
+
+
+
+function animate() {
+  deltaT = clock.getDelta();
+
+  if (rotateX == 1)
+      plane.rotation.x -= 0.05;
+  if (rotateX == 2)
+      plane.rotation.x += 0.05;
+  if (rotateY == 1)
+      plane.rotation.y += 0.05;
+  if (rotateY == 2)
+      plane.rotation.y -= 0.05;
+  rotateX = 0;
+  rotateY = 0;
+  render();
+  requestAnimationFrame(animate);
+}
+
+function init() {
+
+  clock = new THREE.Clock();
+  clock.start();
+  renderer = new THREE.WebGLRenderer( { antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight );
+
+  document.body.appendChild(renderer.domElement);
+
+  createScene();
+  createPerspectiveCamera();
+  createCamera();
+
+  window.addEventListener("resize", onResize);
+  window.addEventListener("keydown", onKeyDown);
+}
